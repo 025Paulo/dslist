4771ce95820ee8231e422a78471176caf9b4ff63
Paulo Batista <paulo.batista025@outlook.comgit config --global user.email paulo.batista025@outlook.comgit config --global user.email paulo.batista025@outlook.com>
2025-01-22 19:04:03 -0300

    GET by id

--- src/main/java/com/_paulo/dslist/services/GameService.java
+++ src/main/java/com/_paulo/dslist/services/GameService.java
@@ -1,10 +1,12 @@
 package com._paulo.dslist.services;
 
+import com._paulo.dslist.dto.GameDTO;
 import com._paulo.dslist.dto.GameMinDTO;
 import com._paulo.dslist.entities.Game;
 import com._paulo.dslist.repositories.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -15,6 +17,14 @@ public class GameService {
     @Autowired
     private GameRepository gameRepository;
 
+    @Transactional(readOnly = true)
+    public GameDTO findById(Long id) {
+        Game result = gameRepository.findById(id).get();
+        GameDTO dto = new GameDTO(result);
+        return dto;
+    }
+
+    @Transactional(readOnly = true)
     public List<GameMinDTO> findAll() {
         List<Game> result = gameRepository.findAll();
         List<GameMinDTO> dto = result.stream().map(x -> new GameMinDTO(x)).toList();
